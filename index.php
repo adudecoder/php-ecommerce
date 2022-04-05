1182d940cd7f82b02a2b0c1ef22a1c8c6121c3f6
Victor Martins <vitorchiesa74@gmail.com>
2022-04-05 11:17:11 -0300

    usuarios read, create, update, delete

--- index.php
+++ index.php
@@ -70,6 +70,103 @@
 
 	});
 
+	$app -> get('/admin/users', function() { // Rota da listagem de todos users
+
+		User::verifyLogin(); // Verificando se está logado
+
+		// Rota para chamar a lista de users do db:
+		$users = User::listAll(); // Criando médoto listAll no Model/User.php retorna array com a lista de users
+
+		$page = new PageAdmin();
+
+		$page -> setTpl("users", array( // Buscando template users e chamando o array
+			"users" => $users
+		));
+
+	});
+
+	// Rota para deletar um user, só altera o método #ATENÇÃO# não usamos o método delete direto por causa do RainTpl, precisamos chamá-lo
+	$app -> get("/admin/users/:iduser/delete", function($iduser) { // Rota para deletar um usúario
+
+		User::verifyLogin();
+
+		$user = new User();
+
+		$user -> get((int)$iduser);
+
+		$user -> delete(); 
+
+		header("Location: /admin/users"); // Criar o método delete em User.php
+		exit;
+
+	});
+
+	$app -> get('/admin/users/create', function() { // Rota do create user (get responde html)
+
+		User::verifyLogin();
+
+		$page = new PageAdmin();
+
+		$page -> setTpl("users-create"); // Buscando template users-create
+
+	});
+
+	// O que vinher em :iduser vai ser recebido na variavel $iduser
+	$app->get("/admin/users/:iduser", function($iduser) // Rota do update users - #ATENÇÃO# estamos chamando o usuário que queremos alterar em :iduser
+	{
+		User::verifyLogin();
+
+		$user = new User(); // Chamando um novo usuário
+
+		$user -> get((int)$iduser);
+
+		$page = new PageAdmin();
+
+		$page -> setTpl("users-update", array( 	// Buscando template users-update
+			"user" => $user -> getValues() // Passamos os valores para a chave "user"
+		));
+
+	});
+
+	$app -> post("/admin/users/create", function() { // Rota para salvar o user criado (post responde inserção de dados)
+
+		User::verifyLogin();
+
+		//// var_dump($_POST); // Conferindo se está recebendo os dados do formulário
+
+		$user = new User(); // Criando o novo usuário
+
+		$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0; // Se inadmin foi definido o valor é 1, se não é 0
+
+		$user -> setData($_POST); // Método setData: cria automaticamente as variáveis no Data Access Object
+
+		$user -> save(); // save() vai executar o INSERT dentro do banco
+
+		header("Location: /admin/users"); // Depois de criar o user redirecionar para a tabela
+		exit;
+
+	});
+
+	$app -> post("/admin/users/:iduser", function($iduser) { // Rota para salvar o user que teve update
+
+		User::verifyLogin();
+
+		$user = new User();
+
+		$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0; // Se inadmin foi definido o valor é 1, se não é 0
+
+		// Trazer tudo do banco para depois alterar:
+		$user -> get((int)$iduser);
+
+		$user -> setData($_POST);
+
+		$user -> update(); // Criando o método update no User.php
+
+		header("Location: /admin/users");
+		exit;
+
+	});
+
 	$app -> run(); // Aqui é executado toda a aplicação
 
 ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -74,6 +74,87 @@
 
         }
 
+        public static function listAll()
+        {
+
+            $sql = new Sql();
+
+            return $sql -> select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+            // Unimos as tabelas users e persons, ordenando pelo nome da pessoa desperson
+
+        }
+
+        public function save() // Método para salvar os dados do new user no db
+        {
+
+            $sql = new Sql();
+
+            // #ATENÇÃO# Chamando o PROCEDURE que vai fazer o INSERT, SELECT automaticamente evitando várias linhas de código e request/response para o servidor:
+            $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+                ":desperson"    => $this -> getdesperson(),
+                ":deslogin"     => $this -> getdeslogin(),
+                ":despassword"  => $this -> getdespassword(),
+                ":desemail"     => $this -> getdesemail(),
+                ":nrphone"      => $this -> getnrphone(),
+                ":inadmin"      => $this -> getinadmin()
+                // Todos esses getters foram gerados pelo setdata
+            ));
+
+            // Só nos interessa a primeira linha desse results, setando no próprio objeto:
+            $this -> setData($results[0]);
+
+        }
+
+        public function get($iduser)
+        {
+         
+            $sql = new Sql();
+            
+            $results = $sql -> select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+            ":iduser" => $iduser
+            ));
+            
+            $data = $results[0];
+            
+            $this -> setData($data);
+         
+        }
+
+        public function update() // Vai ser bem similar ao save()
+        {
+
+            $sql = new Sql();
+
+            // #ATENÇÃO# Chamando o PROCEDURE que vai fazer o INSERT, SELECT automaticamente evitando várias linhas de código e request/response para o servidor:
+            $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+                ":iduser"       => $this -> getiduser(),
+                ":desperson"    => $this -> getdesperson(),
+                ":deslogin"     => $this -> getdeslogin(),
+                ":despassword"  => $this -> getdespassword(),
+                ":desemail"     => $this -> getdesemail(),
+                ":nrphone"      => $this -> getnrphone(),
+                ":inadmin"      => $this -> getinadmin()
+                // Todos esses getters foram gerados pelo setdata
+            ));
+
+            // Só nos interessa a primeira linha desse results, setando no próprio objeto:
+            $this -> setData($results[0]);
+
+        }
+
+        public function delete() // Método para deletar o usuário
+        {
+
+            $sql = new Sql();
+
+            $sql -> query("CALL sp_users_delete(:iduser)", array(
+
+                ":iduser" => $this -> getiduser()
+
+            ));
+
+        }
+
     }
 
 ?>
